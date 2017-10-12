12fc5962d51ce502376c41544a7101f2a95b717b
Daan den Hartog <daandh94@gmail.com>
2017-10-12 14:35:11 +0200

    Set up for controllers

---
+++ Assets/Scripts/Controllers/Controller.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Controller : ScriptableObject {
+
+    public Color color;
+    public string controllerName;
+    public char character;
+
+    public virtual void Initialize() {
+
+    }
+
+    public virtual void OnEnableTurn() {
+
+    }
+
+    public virtual void OnDisableTurn() {
+
+    }
+}
