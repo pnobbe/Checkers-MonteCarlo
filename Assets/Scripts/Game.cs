efc46877a6d61e0c95e655bbcbd83f5fbbf0bff6
Daan den Hartog <daandh94@gmail.com>
2017-10-17 18:48:08 +0200

    Add most ugly ass win checking code

--- Assets/Scripts/Game.cs
+++ Assets/Scripts/Game.cs
@@ -10,10 +10,10 @@ public class Game : MonoBehaviour {
     [HideInInspector]
     public Button[,] buttons = new Button[3, 3];
     [HideInInspector]
-    public char[,] characters = new char[,] { { '0', '0', '0' }, { '0', '0', '0' }, { '0', '0', '0' } };                                            
+    public char[,] characters = new char[,] { { '0', '0', '0' }, { '0', '0', '0' }, { '0', '0', '0' } };
 
     private Controller current;
-    private int turn = 0; 
+    private int turn = 0;
 
     private void Awake() {
         // Get all available buttons, so we can change the text later on
@@ -23,8 +23,8 @@ public class Game : MonoBehaviour {
                 buttons[x, y] = row.Find(string.Format("Tile:{0:00}", x)).GetComponent<Button>();
             }
         }
-        
-        foreach(Controller c in controllers) {
+
+        foreach (Controller c in controllers) {
             c.Initialize(this);
         }
 
@@ -43,13 +43,47 @@ public class Game : MonoBehaviour {
 
         characters[p.x, p.y] = controller.character;
 
+        if (IsHorizontalWin(p.y) || IsVerticalWin(p.x)) {
+            EndGame();
+            return;
+        }
+
+        if (p.x == 1 && p.y == 1) {
+            if (IsDiagonalWin(Point2.zero, Point2.one * 2) || IsDiagonalWin(new Point2(0, 2), new Point2(2, 0))) {
+                EndGame();
+                return;
+            }
+        } else if ((p.x == 0 && p.y == 0) || (p.x == 2 && p.y == 2)) {
+            if (IsDiagonalWin(Point2.zero, Point2.one * 2)) {
+                EndGame();
+                return;
+            }
+        } else if ((p.x == 0 && p.y == 2) || (p.x == 2 && p.y == 0)) {
+            if (IsDiagonalWin(new Point2(0, 2), new Point2(2, 0))) {
+                EndGame();
+                return;
+            }
+        }
+
         turn++;
-        if(turn > 8) {
+        if (turn > 8) {
             Debug.Log("Draw");
             return;
         }
 
         SetNextTurn();
+    }
+
+    private bool IsHorizontalWin(int row) {
+        return characters[0, row] == characters[1, row] && characters[2, row] == characters[1, row] && characters[1, row] != '0';
+    }
+
+    private bool IsVerticalWin(int colum) {
+        return characters[colum, 0] == characters[colum, 1] && characters[colum, 2] == characters[colum, 1] && characters[colum, 1] != '0';
+    }
+
+    private bool IsDiagonalWin(Point2 a, Point2 b) {
+        return characters[a.x, a.y] == characters[1, 1] && characters[b.x, b.y] == characters[1, 1] && characters[1, 1] != '0';
     }
 
     private void SetNextTurn() {
@@ -65,7 +99,11 @@ public class Game : MonoBehaviour {
         text.color = color;
     }
 
-    private void EndGame(string text, Color color) {
+    private void EndGame() {
+        foreach(Controller c in controllers) {
+            c.OnDisableTurn();
+        }
 
+        Debug.Log("It's a win!");
     }
 }
