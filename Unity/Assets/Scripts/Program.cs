44881e617448356d6c269657d08a503114253d38
Daan den Hartog <daandh94@gmail.com>
2017-10-27 18:51:44 +0200

    Add finished screen

--- Unity/Assets/Scripts/Program.cs
+++ Unity/Assets/Scripts/Program.cs
@@ -8,6 +8,7 @@ using UnityEngine.UI;
 namespace MonteCarlo {
     public class Program : MonoBehaviour {
 
+        public GameObject endDisplay;
         public Text text;
 
         private Color cplayer = Color.green;
@@ -18,22 +19,42 @@ namespace MonteCarlo {
         private MonteCarloTreeSearch mcts;
         private Board board;
 
-        private Button[,] buttons = new Button[3,3];
+        private Button[,] buttons = new Button[3, 3];
 
         private const int numOfMCiterations = 17;
-        
-        private void Awake() {
-            mcts = new MonteCarloTreeSearch(numOfMCiterations);
-            board = new Board();
 
+        private int currentStartingPlayer = 2;
+        private int currentPlayer;
+
+        private void Awake() {
             Button[] bs = FindObjectsOfType<Button>();
-            foreach(Button b in bs) {
+            foreach (Button b in bs) {
                 ButtonValue bv = b.GetComponent<ButtonValue>();
                 buttons[bv.x, bv.y] = b;
             }
+
+            NextGame();
+        }
+
+        public void NextGame() {
+            endDisplay.SetActive(false);
+            mcts = new MonteCarloTreeSearch(numOfMCiterations);
+            board = new Board();
+
+            for (int x = 0; x < 3; x++)
+                for (int y = 0; y < 3; y++) {
+                    buttons[x, y].GetComponent<Image>().color = Color.white;
+                }
+
+            currentStartingPlayer = 3 - currentStartingPlayer;
+            currentPlayer = currentStartingPlayer;
+
+            if (currentPlayer == 2) PerformOpponentMove(2);
         }
 
         public void PerformMove(Button b) {
+            if (currentPlayer == 2) return;
+
             ButtonValue bv = b.GetComponent<ButtonValue>();
             board.performMove(1, new Position(bv.x, bv.y));
 
@@ -44,7 +65,10 @@ namespace MonteCarlo {
             Debug.Log(s);
 
             if (s == "Game In progress") {
+                currentPlayer = 2;
                 PerformOpponentMove(2);
+            } else {
+                GameFinished();
             }
         }
 
@@ -56,14 +80,25 @@ namespace MonteCarlo {
 
             string s = board.printStatus();
             Debug.Log(s);
+
+            if (s == "Game In progress") {
+                currentPlayer = 1;
+            } else {
+                GameFinished();
+            }
+        }
+
+        private void GameFinished() {
+            endDisplay.SetActive(true);
+            endDisplay.transform.Find("EndText").GetComponent<Text>().text = board.printStatus();
         }
 
         private void FillText() {
             int[,] b = board.getBoardValues();
             string[] data = new string[] { "", "", "" };
 
-            for(int x = 0; x < 3; x++)
-                for(int y = 0; y < 3; y++) {
+            for (int x = 0; x < 3; x++)
+                for (int y = 0; y < 3; y++) {
                     data[y] += b[x, y] + " ";
                 }
 
@@ -76,7 +111,7 @@ namespace MonteCarlo {
 
             for (int x = 0; x < 3; x++)
                 for (int y = 0; y < 3; y++) {
-                    if(bo[x, y] == 1 || bo[x, y] == 2) {
+                    if (bo[x, y] == 1 || bo[x, y] == 2) {
                         buttons[x, y].GetComponent<Image>().color = bo[x, y] == 1 ? cplayer : copponent;
                     }
                 }
